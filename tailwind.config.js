96fb129a35fd133089cb6ca3499836204084978d
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-08-29 11:42:50 +0530

    ui added

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      offwhite: "#fcfeff",
+      skyBlue: "#87CEEB",
+      mintGreen: "#98FF98",
+      lavender: "#E6E6FA",
+      steelGray: "#6A6E7F",
+      slateBlue: "#6A5ACD",
+      teal: "#008080",
+      dustyRose: "#D4A1BB",
+      coolGray: "#B0B7C6",
+      paleAqua: "#B3E0E4",
+      lilac: "#C8A2C8",
+      periwinkle: "#CCCCFF",
+      powderBlue: "#B0E0E6",
+      smokyLavender: "#A899E6",
+      icyMint: "#AEEEEE",
+      mutedBlue: "#5C85D6",
+  },
+  fontFamily: {
+    mullish: ["Mulish", "sans-serif"],
+  },
+  
+    extend: {},
+  },
+
+  plugins: [],
+}
+
